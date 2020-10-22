de8d4fa3e1170c26552cbf0429a85933315ddb97
SanathNarayana <68953929+SanathNarayana@users.noreply.github.com>
2020-10-22 20:33:58 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var dog, happyDog;
+var database;
+var foodS,foodStock;
+function preload()
+{
+ dog=loadImage("dogImg.png");
+ happyDog.loadImage("dogImg1.png");
+}
+
+function setup() {
+	createCanvas(800, 700);
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
